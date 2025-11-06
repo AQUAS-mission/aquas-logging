dce234bfd2a5c4c6ccfaba16df1b93ba9780f06e
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2025-11-05 21:34:08 -0500

    remove sidebar

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -174,7 +174,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
       </SidebarFooter>
     </Sidebar>
   )
